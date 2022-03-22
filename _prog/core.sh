a72a15f95a7ff873bafbea5bc4d121d954d53208
mirage335 <spamfreemirage335@gmail.com>
2022-03-22 16:52:47 -0400

    WIP.

--- _prog/core.sh
+++ _prog/core.sh
@@ -1,2 +1,70 @@
 ##### Core
 
+
+_test_build_cloud() {
+	_getDep wget
+	_getDep axel
+	
+	_getDep gcc
+	_getDep g++
+	_getDep make
+	
+	_getDep ld
+	
+	_getDep patch
+	
+	_getDep gpg
+	
+	_getDep tar
+	_getDep xz
+	_getDep gzip
+	_getDep bzip2
+	
+	_getDep pahole
+	
+	_getDep lz4
+	_getDep lz4c
+	
+	_test_kernelConfig
+}
+
+_build_cloud() {
+	local functionEntryPWD
+	functionEntryPWD="$PWD"
+	_start
+	
+	_test_build_cloud
+	"$scriptAbsoluteLocation" _setupUbiquitous_nonet
+	
+	
+	
+	
+	
+	local kernelURL
+	local kernelName
+	mkdir -p "$scriptLocal"/lts
+	cd "$scriptLocal"/lts
+	#kernelURL="https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.10.61.tar.xz"
+	kernelURL=$(wget -q -O - 'https://kernel.org/' | grep https | grep 'tar\.xz' | grep '5\.10' | sed 's/^.*https/https/' | sed 's/.tar.xz.*$/\.tar\.xz/' | tr -dc 'a-zA-Z0-9.:\=\_\-/%')
+	kernelName=$(_safeEcho_newline "$kernelURL" | sed 's/^.*\///' | sed 's/\.tar\.xz$//')
+	
+	wget "$kernelURL"
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	mkdir -p "$scriptLocal"/mainline
+	
+	
+	
+	_stop
+	cd "$functionEntryPWD"
+}
+
--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -32,7 +32,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='1891409836'
-export ub_setScriptChecksum_contents='4204851865'
+export ub_setScriptChecksum_contents='1462654538'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -35545,6 +35545,74 @@ _package() {
 
 
 ##### Core
+
+
+_test_build_cloud() {
+	_getDep wget
+	_getDep axel
+	
+	_getDep gcc
+	_getDep g++
+	_getDep make
+	
+	_getDep ld
+	
+	_getDep patch
+	
+	_getDep gpg
+	
+	_getDep tar
+	_getDep xz
+	_getDep gzip
+	_getDep bzip2
+	
+	_getDep pahole
+	
+	_getDep lz4
+	_getDep lz4c
+	
+	_test_kernelConfig
+}
+
+_build_cloud() {
+	local functionEntryPWD
+	functionEntryPWD="$PWD"
+	_start
+	
+	_test_build_cloud
+	"$scriptAbsoluteLocation" _setupUbiquitous_nonet
+	
+	
+	
+	
+	
+	local kernelURL
+	local kernelName
+	mkdir -p "$scriptLocal"/lts
+	cd "$scriptLocal"/lts
+	#kernelURL="https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.10.61.tar.xz"
+	kernelURL=$(wget -q -O - 'https://kernel.org/' | grep https | grep 'tar\.xz' | grep '5\.10' | sed 's/^.*https/https/' | sed 's/.tar.xz.*$/\.tar\.xz/' | tr -dc 'a-zA-Z0-9.:\=\_\-/%')
+	kernelName=$(_safeEcho_newline "$kernelURL" | sed 's/^.*\///' | sed 's/\.tar\.xz$//')
+	
+	wget "$kernelURL"
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	mkdir -p "$scriptLocal"/mainline
+	
+	
+	
+	_stop
+	cd "$functionEntryPWD"
+}
 
 
 #####Program
