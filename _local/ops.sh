e5b1611b45d1c0fe9f4fdcca5f0fb09985648105
mirage335 <spamfreemirage335@gmail.com>
2022-03-27 23:16:54 -0400

    Experiment.

--- _local/ops.sh
+++ _local/ops.sh
@@ -10,282 +10,177 @@ export ub_anchor_autoupgrade="true"
 
 
 
-
-
-
-_test_sanity() {
-	if (exit 0)
-	then
-		true
-	else
-		_messageFAIL && return 1
-	fi
-	if ! (exit 0)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 1)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 2)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 3)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 126)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 127)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 128)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 129)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 130)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 131)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 132)
-	then
-		_messageFAIL && return 1
-	fi
-	if (exit 255)
-	then
-		_messageFAIL && return 1
-	fi
+_variableLocalTestA_procedure() {
+	local currentLocalA
+	currentLocalA='false'
+	[[ "$currentGlobalA" != 'true' ]] && _stop 1
+	[[ "$currentLocalA" == '' ]] && _stop 1
+	[[ "$currentLocalA" != 'false' ]] && _stop 1
 	
-	local currentSubReturnStatus
-	(exit 0)
-	currentSubReturnStatus="$?"
-	[[ "$currentSubReturnStatus" != '0' ]] && _messageFAIL && return 1
-	(exit 1)
-	currentSubReturnStatus="$?"
-	[[ "$currentSubReturnStatus" != '1' ]] && _messageFAIL && return 1
-	(exit 2)
-	currentSubReturnStatus="$?"
-	[[ "$currentSubReturnStatus" != '2' ]] && _messageFAIL && return 1
-	(exit 3)
-	currentSubReturnStatus="$?"
-	[[ "$currentSubReturnStatus" != '3' ]] && _messageFAIL && return 1
+	local currentLocalB
+	currentLocalB='true'
 	
-	
-	# Do NOT allow 'rm' to be a shell function alias to 'rm -i' or similar.
-	[[ $(type -p rm) == "" ]] && _messageFAIL && return 1
-	
-	#! [[ -2147483648 -lt 2147483647 ]] && _messageFAIL && return 1
-	#! [[ -2000000000 -lt 2000000000 ]] && _messageFAIL && return 1
-	
-	! [[ -1234567890 -le -1234567890 ]] && _messageFAIL && return 1
-	! [[ 1234567890 -le 1234567890 ]] && _messageFAIL && return 1
-	! [[ -1234567890 -lt 1234567890 ]] && _messageFAIL && return 1
-	! [[ -1234567890 -ge -1234567890 ]] && _messageFAIL && return 1
-	! [[ 1234567890 -ge 1234567890 ]] && _messageFAIL && return 1
-	! [[ 1234567890 -gt -1234567890 ]] && _messageFAIL && return 1
-	! [[ -1234567890 -lt -0 ]] && _messageFAIL && return 1
-	! [[ -1234567890 -lt 0 ]] && _messageFAIL && return 1
-	! [[ 0 -lt 1234567890 ]] && _messageFAIL && return 1
-	! [[ -0 -gt -1234567890 ]] && _messageFAIL && return 1
-	! [[ 0 -gt -1234567890 ]] && _messageFAIL && return 1
-	! [[ 0 -gt -1234567890 ]] && _messageFAIL && return 1
-	
-	! [[ -900000000 -le -900000000 ]] && _messageFAIL && return 1
-	! [[ 900000000 -le 900000000 ]] && _messageFAIL && return 1
-	! [[ -900000000 -lt 900000000 ]] && _messageFAIL && return 1
-	! [[ -900000000 -ge -900000000 ]] && _messageFAIL && return 1
-	! [[ 900000000 -ge 900000000 ]] && _messageFAIL && return 1
-	! [[ 900000000 -gt -900000000 ]] && _messageFAIL && return 1
-	! [[ -900000000 -lt -0 ]] && _messageFAIL && return 1
-	! [[ -900000000 -lt 0 ]] && _messageFAIL && return 1
-	! [[ 0 -lt 900000000 ]] && _messageFAIL && return 1
-	! [[ -0 -gt -900000000 ]] && _messageFAIL && return 1
-	! [[ 0 -gt -900000000 ]] && _messageFAIL && return 1
-	! [[ 0 -gt -900000000 ]] && _messageFAIL && return 1
-	! [[ 0 -le -0 ]] && _messageFAIL && return 1
-	! [[ -0 -le 0 ]] && _messageFAIL && return 1
-	! [[ 0 -ge -0 ]] && _messageFAIL && return 1
-	! [[ -0 -ge 0 ]] && _messageFAIL && return 1
-	
-	
-	
-	! "$scriptAbsoluteLocation" _true && _messageFAIL && return 1
-	"$scriptAbsoluteLocation" _false && _messageFAIL && return 1
-	
-	# CAUTION: Profoundly unexpected to have called '_test' or similar functions after importing into a current shell in any way.
-	if ( [[ "$current_internal_CompressedScript" == "" ]] && [[ "$current_internal_CompressedScript_cksum" == "" ]] && [[ "$current_internal_CompressedScript_bytes" == "" ]] ) || ( ( [[ "$ub_import_param" != "--embed" ]] ) && [[ "$ub_import_param" != "--bypass" ]] && [[ "$ub_import_param" != "--call" ]] && [[ "$ub_import_param" != "--script" ]] && [[ "$ub_import_param" != "--compressed" ]] )
-	then
-		[[ "$ub_import" == 'true' ]] && _messageFAIL && _stop 1
-		[[ "$ub_import" != '' ]] && _messageFAIL && _stop 1
-		[[ "$ub_import_param" != '' ]] && _messageFAIL && _stop 1
-	fi
-	
-	local santiySessionID_length
-	santiySessionID_length=$(echo -n "$sessionid" | wc -c | tr -dc '0-9')
-	
-	[[ "$santiySessionID_length" -lt "18" ]] && _messageFAIL && return 1
-	[[ "$uidLengthPrefix" != "" ]] && [[ "$santiySessionID_length" -lt "$uidLengthPrefix" ]] && _messageFAIL && return 1
-	
-	[[ -e "$safeTmp" ]] && _messageFAIL && return 1
-	
-	_start scriptLocal_mkdir_disable
-	
-	
-	[[ ! -e "$safeTmp" ]] && _messageFAIL && return 1
-	
-	[[ $( cd "$safeTmp" 2>/dev/null ; ls *doNotMatch* 2>/dev/null | wc -c) != "0" ]] && _messageFAIL && return 1
-	[[ $( cd "$safeTmp" 2>/dev/null ; ls -A *doNotMatch* 2>/dev/null | wc -c) != "0" ]] && _messageFAIL && return 1
-	
-	echo -e -n >> "$safeTmp"/empty
-	[[ ! -e "$safeTmp"/empty ]] && _messageFAIL && return 1
-	[[ $(cat "$safeTmp"/empty | wc -c) != '0' ]] && _messageFAIL && return 1
-	
-	[[ $( cd "$safeTmp" 2>/dev/null ; ls *empty* 2>/dev/null | wc -c) == "0" ]] && _messageFAIL && return 1
-	[[ $( cd "$safeTmp" 2>/dev/null ; ls -A *empty* 2>/dev/null | wc -c) == "0" ]] && _messageFAIL && return 1
-	
-	rm -f "$safeTmp"/empty > /dev/null 2>&1
-	
-	
-	! _test_moveconfirm_procedure && _messageFAIL && return 1
-	
-	
-	local currentTestUID=$(_uid 245)
-	mkdir -p "$safeTmp"/"$currentTestUID"
-	echo > "$safeTmp"/"$currentTestUID"/"$currentTestUID"
-	
-	[[ ! -e "$safeTmp"/"$currentTestUID"/"$currentTestUID" ]] && _messageFAIL && return 1
-	
-	rm -f "$safeTmp"/"$currentTestUID"/"$currentTestUID"
-	rmdir "$safeTmp"/"$currentTestUID"
-	
-	[[ -e "$safeTmp"/"$currentTestUID" ]] && _messageFAIL && return 1
-	
-	echo 'true' > "$safeTmp"/shouldNotOverwrite
-	mv "$safeTmp"/doesNotExist "$safeTmp"/shouldNotOverwrite > /dev/null 2>&1 && _messageFAIL && return 1
-	echo > "$safeTmp"/replacement
-	mv -n "$safeTmp"/replacement "$safeTmp"/shouldNotOverwrite > /dev/null 2>&1
-	[[ $(cat "$safeTmp"/shouldNotOverwrite) != "true" ]] && _messageFAIL && return 1
-	rm -f "$safeTmp"/replacement > /dev/null 2>&1
-	rm -f "$safeTmp"/shouldNotOverwrite > /dev/null 2>&1
-	
-	
-	_uid_test
-	
-	[[ $(_getUUID | wc -c) != '37' ]] && _messageFAIL && return 1
-	
-	[[ $(_getUUID | cut -f1 -d\- | wc -c) != '9' ]] &&  _messageFAIL && return 1
-	
-	
-	! env | grep 'PATH' > /dev/null 2>&1 && _messageFAIL && return 1
-	! printenv | grep 'PATH' > /dev/null 2>&1 && _messageFAIL && return 1
-	
-	echo test1
-	_define_function_test
-	echo test2
-	! _variableLocalTest && _messageFAIL && return 1
-	echo test3
-	
-	
-	mkdir -p "$safeTmp"/maydeletethisfolder
-	[[ ! -d "$safeTmp"/maydeletethisfolder ]] && return 1
-	echo > "$safeTmp"/maydeletethisfolder/maydeletethisfile
-	[[ ! -e "$safeTmp"/maydeletethisfolder/maydeletethisfile ]] && return 1
-	echo test4
-	_safeRMR "$safeTmp"/maydeletethisfolder
-	echo test5
-	[[ -e "$safeTmp"/maydeletethisfolder/maydeletethisfile ]] && return 1
-	[[ -e "$safeTmp"/maydeletethisfolder ]] && return 1
-	echo test6
-	
-	
-	
-	# WARNING: Not tested by default, due to lack of use except where faults are tolerable, and slim possibility of useful embedded systems not able to pass.
-	#! echo \$123 | grep -E '^\$[0-9]|^\.[0-9]' > /dev/null 2>&1 && _messageFAIL && return 1
-	#! echo \.123 | grep -E '^\$[0-9]|^\.[0-9]' > /dev/null 2>&1 && _messageFAIL && return 1
-	#echo 123 | grep -E '^\$[0-9]|^\.[0-9]' > /dev/null 2>&1 && _messageFAIL && return 1
-	
-	
-	local currentJobsList
-	currentJobsList=$(jobs -p -r)
-	
-	[[ "$currentJobsList" != "" ]] && return 1
-	
-	sleep 7 &
-	currentJobsList=$(jobs -p -r)
-	[[ "$currentJobsList" == "" ]] && return 1
-	
-	wait
-	currentJobsList=$(jobs -p -r)
-	[[ "$currentJobsList" != "" ]] && return 1
-	
-	
-	
-	
-	if ! _test_embed
-	then
-		_messageFAIL && _stop 1
-		#! uname -a | grep -i cygwin > /dev/null 2>&1 && _messageFAIL && _stop 1
-		#echo 'warn: broken (cygwin): _test_embed - cygwin detected'
-	fi
-	
-	
-	
-	
-	_getDep flock
-	
-	( flock 200; echo > "$safeTmp"/ready ; sleep 3 ) 200>"$safeTmp"/flock &
-	sleep 1
-	if ( flock 200; ! [[ -e "$safeTmp"/ready ]] ) 200>"$safeTmp"/flock
-	then
-		! uname -a | grep -i cygwin > /dev/null 2>&1 && _messageFAIL && _stop 1
-		echo 'warn: broken (cygwin): flock - cygwin may not be able to use flock through MSW network drive'
-		return 1
-	fi
-	rm -f "$safeTmp"/flock > /dev/null 2>&1
-	rm -f "$safeTmp"/ready > /dev/null 2>&1
-	
-	ln -s /dev/null "$safeTmp"/working
-	ln -s /dev/null/broken "$safeTmp"/broken
-	if ! [[ -h "$safeTmp"/broken ]] || ! [[ -h "$safeTmp"/working ]] || [[ -e "$safeTmp"/broken ]] || ! [[ -e "$safeTmp"/working ]]
-	then
-		! uname -a | grep -i cygwin > /dev/null 2>&1 && _messageFAIL && _stop 1
-		echo 'warn: broken (cygwin): flock - cygwin may not be able to use flock through MSW network drive'
-		return 1
-	fi
-	rm -f "$safeTmp"/working
-	rm -f "$safeTmp"/broken
-	
-	
-	
-	_tryExec _test_parallelFifo_procedure
+	currentNotLocalA='true'
 	
 	
 	return 0
 }
 
+_variableLocalTestB_procedure() {
+	[[ "$currentGlobalA" != 'true' ]] && return 1
+	[[ "$currentLocalA" != '' ]] && return 1
+	[[ "$currentLocalA" == 'true' ]] && return 1
+	
+	return 0
+}
+
+_variableLocalTestC_procedure() {
+	[[ "$currentGlobalA" != '' ]] && _stop 1
+	[[ "$currentGlobalA" == 'true' ]] && _stop 1
+	
+	return 0
+}
+
+_variableLocalTest_sequence() {
+	echo test2
+	_start scriptLocal_mkdir_disable
+	echo test3
+	variableLocalTest_currentSubFunction() {
+		if ! [[ "$currentSubFunctionTest" == "true" ]] || ! [[ $(echo "$currentSubFunctionTest") == "true" ]]
+		then
+			_stop 1
+			return 1
+		fi
+		return 0
+	}
+	local currentSubFunctionTest
+	currentSubFunctionTest='true'
+	! variableLocalTest_currentSubFunction && _stop 1
+	echo test4
+	
+	local currentSubshellTest1=$(
+		echo x
+	)
+	[[ "$currentSubshellTest1" != 'x' ]] && _stop 1
+	
+	local currentSubshellTest2
+	currentSubshellTest2=$(
+		echo x
+	)
+	[[ "$currentSubshellTest2" != 'x' ]] && _stop 1
+	
+	echo test5
+	echo $(
+		echo 1
+		echo 2
+	) | grep '1 2' > /dev/null || _stop 1
+	
+	! echo $(
+		echo 1
+		echo 2
+	) | grep '1 2' > /dev/null && _stop 1
+	
+	
+	export currentGlobalA='true'
+	
+	local currentLocalA
+	currentLocalA='true'
+	
+	( export currentSubshellTestA='true' )
+	[[ ! -z "$currentSubshellTestA" ]] && _stop 1
+	[[ "$currentSubshellTestA" != "" ]] && _stop 1
+	[[ "$currentSubshellTestA" != '' ]] && _stop 1
+	[[ "$currentSubshellTestA" == 'true' ]] && _stop 1
+	
+	( currentSubshellTestB='true' )
+	[[ "$currentSubshellTestB" != "" ]] && _stop 1
+	[[ "$currentSubshellTestB" == 'true' ]] && _stop 1
+	
+	( local currentSubshellTestC='true' )
+	[[ "$currentSubshellTestC" != "" ]] && _stop 1
+	[[ "$currentSubshellTestC" == 'true' ]] && _stop 1
+	echo test6
+	! ( echo true ) | grep 'true' > /dev/null && _stop 1
+	! ( echo "$currentGlobalA" ) | grep 'true' > /dev/null && _stop 1
+	! ( echo "$currentLocalA" ) | grep 'true' > /dev/null && _stop 1
+	( echo "$currentLocalB" ) | grep 'true' > /dev/null && _stop 1
+	
+	[[ "$currentLocalA" != 'true' ]] && _stop 1
+	! _variableLocalTestA_procedure && _stop 1
+	[[ "$currentLocalA" != 'true' ]] && _stop 1
+	[[ "$currentLocalB" != '' ]] && _stop 1
+	[[ "$currentLocalB" == 'true' ]] && _stop 1
+	[[ "$currentNotLocalA" != 'true' ]] && _stop 1
+	
+	_variableLocalTestB_procedure && _stop 1
+	! "$scriptAbsoluteLocation" _variableLocalTestB_procedure && _stop 1
+	
+	local currentGlobalA
+	! _variableLocalTestC_procedure && _stop 1
+	
+	export currentGlobalB='false'
+	local currentGlobalB='true'
+	[[ "$currentGlobalB" != 'true' ]] && _stop 1
+	
+	local currentLocalB='true'
+	! ( echo "$currentLocalB" ) | grep 'true' > /dev/null && _stop 1
+	[[ "$currentLocalB" != 'true' ]] && _stop 1
+	
+	local currentLocalC='true'
+	[[ "$currentLocalC" != 'true' ]] && _stop 1
+	echo test7
+	! env -i  HOME="$HOME" TERM="${TERM}" SHELL="${SHELL}" PATH="${PATH}" PWD="$PWD" scriptAbsoluteLocation="$scriptAbsoluteLocation" scriptAbsoluteFolder="$scriptAbsoluteFolder" sessionid="$sessionid" LD_PRELOAD="$LD_PRELOAD" USER="$USER" "bash" -c '[[ "$sessionid" != "" ]]' && _stop 1
+	env -i  HOME="$HOME" TERM="${TERM}" SHELL="${SHELL}" PATH="${PATH}" PWD="$PWD" scriptAbsoluteLocation="$scriptAbsoluteLocation" scriptAbsoluteFolder="$scriptAbsoluteFolder" sessionid="" LD_PRELOAD="$LD_PRELOAD" USER="$USER" "bash" -c '[[ "$sessionid" != "" ]]' && _stop 1
+	env -i HOME="$HOME" TERM="${TERM}" SHELL="${SHELL}" PATH="${PATH}" PWD="$PWD" scriptAbsoluteLocation="$scriptAbsoluteLocation" scriptAbsoluteFolder="$scriptAbsoluteFolder" LD_PRELOAD="$LD_PRELOAD" USER="$USER" "bash" -c '[[ "$sessionid" != "" ]]' && _stop 1
+	
+	local currentBashBinLocation
+	currentBashBinLocation=$(type -p bash)
+	[[ "$sessionid" == '' ]] &&  _stop 1
+	! env -i sessionid="$sessionid" "$currentBashBinLocation" -c '[[ "$sessionid" != "" ]]' && _stop 1
+	env -i sessionid="" "$currentBashBinLocation" -c '[[ "$sessionid" != "" ]]' && _stop 1
+	env -i "$currentBashBinLocation" -c '[[ "$sessionid" != "" ]]' && _stop 1
+	
+	
+	
+	local currentVariableFunctionText
+	
+	export UB_TEST_VARIABLE_FUNCTION_COMMAND='_variableFunction_variableLocalTest() { echo true; }'' ; _variableFunction_variableLocalTest'
+	currentVariableFunctionText=$(bash -c "$UB_TEST_VARIABLE_FUNCTION_COMMAND")
+	#currentVariableFunctionText=$($UB_TEST_VARIABLE_FUNCTION_COMMAND)
+	[[ "$currentVariableFunctionText" != "true" ]] && _messageFAIL && _stop 1
+	export UB_TEST_VARIABLE_FUNCTION_COMMAND=
+	unset UB_TEST_VARIABLE_FUNCTION_COMMAND
+	echo test8
+	_exportFunction_variableLocalTest() {
+		echo true
+	}
+	currentVariableFunctionText=$(bash -c '_exportFunction_variableLocalTest' 2>/dev/null)
+	[[ "$currentVariableFunctionText" != "" ]] && _messageFAIL && _stop 1
+	export -f _exportFunction_variableLocalTest
+	currentVariableFunctionText=$(bash -c '_exportFunction_variableLocalTest' 2>/dev/null)
+	[[ "$currentVariableFunctionText" != "true" ]] && _messageFAIL && _stop 1
+	unset _exportFunction_variableLocalTest
+	
+	
+	_currentFunctionDefinitionTest() { echo false; }
+	true && _currentFunctionDefinitionTest() { echo true; }
+	false && _currentFunctionDefinitionTest() { echo false; }
+	[[ $(_currentFunctionDefinitionTest) != "true" ]] && _messageFAIL && _stop 1
+	unset _currentFunctionDefinitionTest
+	echo test9
+	_stop
+}
 
 
-
-
-
-
-
-
-
-
-
+_variableLocalTest() {
+	echo test1
+	if "$scriptAbsoluteLocation" _variableLocalTest_sequence "$@"
+	then
+		echo test95
+		return 0
+	fi
+	echo test96
+	return 1
+}
 
 
