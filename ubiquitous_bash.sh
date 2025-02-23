929bc33f9bfc11de45976cd9e4a3aa2c4b84c9d7
mirage335 <spamfreemirage335@gmail.com>
2025-02-22 21:43:06 -0500

    Upstream.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -36,7 +36,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='2591634041'
-export ub_setScriptChecksum_contents='1537401414'
+export ub_setScriptChecksum_contents='2382020892'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -13895,6 +13895,19 @@ _kernelConfig_require-convenience() {
 	true
 }
 
+_kernelConfig_require-embedded() {
+	_messagePlain_nominal 'kernelConfig: embedded'
+	export kernelConfig_file="$1"
+
+	# Inspired by discussion with Chris Lombardi .
+	#  https://github.com/clearchris
+	# https://github.com/torvalds/linux/commit/24bc41b4558347672a3db61009c339b1f5692169
+	_kernelConfig_warn-y_m CAN_GS_USB
+	_kernelConfig_warn-y__ CAN_RX_OFFLOAD
+
+	true
+}
+
 _kernelConfig_require-special() {
 	_messagePlain_nominal 'kernelConfig: special'
 	export kernelConfig_file="$1"
@@ -14088,6 +14101,8 @@ _kernelConfig_panel() {
 	_kernelConfig_require-investigation "$@"
 	
 	_kernelConfig_require-convenience "$@"
+
+	_kernelConfig_require-embedded "$@"
 	
 	_kernelConfig_require-special "$@"
 	
@@ -14128,6 +14143,8 @@ _kernelConfig_mobile() {
 	_kernelConfig_require-investigation "$@"
 	
 	_kernelConfig_require-convenience "$@"
+
+	_kernelConfig_require-embedded "$@"
 	
 	_kernelConfig_require-special "$@"
 	
@@ -14169,6 +14186,8 @@ _kernelConfig_desktop() {
 	_kernelConfig_require-investigation "$@"
 	
 	_kernelConfig_require-convenience "$@"
+
+	_kernelConfig_require-embedded "$@"
 	
 	_kernelConfig_require-special "$@"
 	
